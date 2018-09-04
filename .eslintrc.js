8d164e7e2fb89fd34201e081b7d5cc3238ad6db9
Ozarion <cozarion@gmail.com>
2018-09-05 00:21:23 +0500

    Eslint is annoying

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,18 +13,6 @@ module.exports = {
         "indent": [
             "error",
             "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
         ]
     }
 };
