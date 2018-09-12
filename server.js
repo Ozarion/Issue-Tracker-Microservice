8bf4e18f3dbaeca1400023b4a79b47784b3933be
Ozarion <cozarion@gmail.com>
2018-09-13 00:03:29 +0500

    added security features

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
-
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+const helmet      = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+// XSS Protection and other security measures
+app.use(helmet());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
